b14c5daa9aebabbc1c84075dad42722b1a381a40
Stefan Oltmann <github@stefan-oltmann.de>
2023-08-02 18:30:29 +0200

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     id("org.jetbrains.kotlinx.kover") version "0.6.1"
     id("com.asarkar.gradle.build-time-tracker") version "4.3.0"
     id("me.qoomon.git-versioning") version "6.4.1"
-    id("com.goncalossilva.resources") version "0.3.2"
+    id("com.goncalossilva.resources") version "0.4.0"
     id("com.github.ben-manes.versions") version "0.47.0"
 }
 
