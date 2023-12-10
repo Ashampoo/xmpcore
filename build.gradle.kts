ce7572b9579da1b1ae78d23fe1c8d7a831c669c4
Stefan Oltmann <github@stefan-oltmann.de>
2023-12-10 08:48:08 +0100

    Configuration fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ sonar {
 detekt {
     source.from("src", "build.gradle.kts")
     allRules = true
-    config.setFrom("$projectDir/../detekt.yml")
+    config.setFrom("$projectDir/detekt.yml")
     parallel = true
     ignoreFailures = true
     autoCorrect = true
