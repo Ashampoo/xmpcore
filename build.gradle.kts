1694fb5e35579a90368560cab041a1d44464b5f6
Stefan Oltmann <github@stefan-oltmann.de>
2024-02-20 13:07:26 +0100

    Removed commented out code

--- build.gradle.kts
+++ build.gradle.kts
@@ -418,12 +418,3 @@ publishing {
     }
 }
 // endregion
-
-//rootProject.the<NodeJsRootExtension>().apply {
-//    nodeVersion = "21.0.0-v8-canary202309143a48826a08"
-//    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
-//}
-//
-//tasks.withType<org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask>().configureEach {
-//    args.add("--ignore-engines")
-//}
