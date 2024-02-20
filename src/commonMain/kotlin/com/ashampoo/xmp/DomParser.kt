c12814a9e2db155a1b82dc44547ff5af9ec4b197
Stefan Oltmann <github@stefan-oltmann.de>
2024-02-20 13:44:13 +0100

    DomParser: Trim everything outside the RDF tags, because so many XMP files have junk there.

--- src/commonMain/kotlin/com/ashampoo/xmp/DomParser.kt
+++ src/commonMain/kotlin/com/ashampoo/xmp/DomParser.kt
@@ -8,19 +8,29 @@ import nl.adaptivity.xmlutil.xmlStreaming
 
 object DomParser {
 
+    private const val RDF_RDF_END = "</rdf:RDF>"
+
     fun parseDocumentFromString(input: String): Document {
 
         /*
-         * We encountered a situation where the XMP had NUL characters at the end
-         * for unknown reasons. This  caused an exception in the parser.
-         * The test images IMG_0001.jpg and IMG_0002.jpg on the iOS simulator
-         * exhibited this issue, indicating that it could occur in real-world
-         * scenarios as well. To address this, we now trim all whitespaces and
-         * ISO control characters from the XMP to ensure its proper parsing.
+         * We encountered a situation where NUL characters at the end of XMP
+         * caused an exception in the parser for unknown reasons. This issue was
+         * observed in the test images IMG_0001.jpg and IMG_0002.jpg on the iOS simulator,
+         * suggesting the possibility of real-world scenarios.
+         *
+         * Additionally, we identified some corrupted files with random junk after the
+         * first </rdf:RDF>, potentially caused by faulty writers.
+         *
+         * Since "xpacket" and "xmpmeta" are skippable at the user's discretion,
+         * the only required tags are within the RDF part. Therefore, we trim it down to this.
          */
-        val trimmedInput = input.trim {
-            it.isWhitespace() || it.isISOControl()
-        }
+
+        val rdfStartPos = input.indexOf("<rdf:RDF")
+        val rfdEndPos = input.indexOf(RDF_RDF_END)
+
+        val trimmedInput = input.substring(
+            rdfStartPos until rfdEndPos + RDF_RDF_END.length
+        )
 
         try {
 
