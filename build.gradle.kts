3c28da16c036010865d52b7070d17a48109dbb6b
Stefan Oltmann <github@stefan-oltmann.de>
2024-03-24 13:54:43 +0100

    Build with Java 17, but target Java 11.

--- build.gradle.kts
+++ build.gradle.kts
@@ -147,8 +147,8 @@ kotlin {
     jvm {
 
         java {
-            sourceCompatibility = JavaVersion.VERSION_17
-            targetCompatibility = JavaVersion.VERSION_17
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
         }
     }
 
